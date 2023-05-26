6c4a2b55cac49004810aa8fc7bdc35dc76324b20
Jose Alberto <josealbertozm@hotmail.com>
2023-05-26 11:02:42 -0700

    fix transform

--- src/common/DTO/jobs/jobs.paginate.ts
+++ src/common/DTO/jobs/jobs.paginate.ts
@@ -1,11 +1,14 @@
+import { Transform } from 'class-transformer'
 import { IsInt, IsNotEmpty, Min } from 'class-validator'
 
 export class JobsPaginate {
+  @Transform(({ value }) => parseInt(value))
   @IsInt()
   @Min(1)
   @IsNotEmpty()
   page: number
 
+  @Transform(({ value }) => parseInt(value))
   @IsInt()
   @Min(1)
   @IsNotEmpty()
